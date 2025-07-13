ea1b26c348b580656c9aa4a06aa4d7d8033bdcb4
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-07-13 16:51:46 -0400

    Update mac-apfs-patch.sh

--- mac-apfs-patch.sh
+++ mac-apfs-patch.sh
@@ -17,7 +17,28 @@ sudo mount -urw /Volumes/System
 cd /Volumes/System/System/Library/Caches/com.apple.dyld
 sudo cp dyld_shared_cache_arm64e dyld_shared_cache_arm64e.orig
 sudo bash -c "$(curl -s https://raw.githubusercontent.com/ChefKissInc/QEMUAppleSiliconTools/master/PatchDYLD.sh)" # Use PatchDYLD.fish for fish shell
+sudo plutil -convert xml1 /Volumes/System/System/Library/xpc/launchd.plist
 
+services=(
+  "com.apple.voicemail.vmd"
+  "com.apple.CommCenter"
+  "com.apple.locationd"
+)
+
+for s in "${services[@]}"; do
+  esc_s=$(printf '%s\n' "$s" | sed 's:/:\\/:g')
+
+  sudo sed -i '' "/<key>${esc_s}<\/key>/ {
+    n
+    /<dict>/ {
+      n
+      i\\
+			<key>Disabled</key>\\
+			<true/>
+    }
+  }" /Volumes/System/System/Library/xpc/launchd.plist
+  
+done
 cd
 diskutil eject /Volumes/System
 
