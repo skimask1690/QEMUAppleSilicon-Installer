f7b1e2cfc40a0b225dae2d86440141d656ee40ad
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-07-25 21:05:38 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,7 +3,7 @@
 # ===== URL VARIABLES =====
 IPSW_14_BETA5_URL="https://updates.cdn-apple.com/2020SummerSeed/fullrestores/001-35886/5FE9BE2E-17F8-41C8-96BB-B76E2B225888/iPhone11,8,iPhone12,1_14.0_18A5351d_Restore.ipsw"
 TICKET_URL="https://raw.githubusercontent.com/ChefKissInc/QEMUAppleSiliconTools/master/ticket.shsh2"
-APTICKET_SCRIPT_URL="https://raw.githubusercontent.com/ChefKissInc/QEMUAppleSiliconTools/master/create_apticket.py"
+APTICKET_PY_URL="https://raw.githubusercontent.com/ChefKissInc/QEMUAppleSiliconTools/master/create_apticket.py"
 IDR_PATCH_URL="https://raw.githubusercontent.com/ChefKissInc/QEMUAppleSiliconTools/master/idevicerestore.patch"
 LIBIMOBILEDEVICE_REPO_BASE="https://github.com/libimobiledevice"
 
