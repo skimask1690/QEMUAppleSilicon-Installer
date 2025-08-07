3954eafe64d92b485c699f1fe130b81496dc48f2
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-08-06 20:35:25 -0400

    Update ios-setup.sh

--- ios-setup.sh
+++ ios-setup.sh
@@ -129,9 +129,9 @@ echo "Starting Companion VM (USB server)..."
 $SU_FLAG ./qemu-system-x86_64 $KVM_FLAG -m 2G -cdrom archlinux-x86_64.iso \
   -drive file=archvm.qcow2,format=qcow2,if=virtio \
   -nic user,model=virtio-net-pci,hostfwd=tcp::32222-:22 \
-  -usb -device usb-ehci,id=ehci -device usb-tcp-remote,conn-type=ipv4,conn-addr=127.0.0.1,conn-port=8030,bus=ehci.0 &
+  -usb -device usb-ehci,id=ehci -device usb-tcp-remote,conn-type=ipv4,conn-addr=127.0.0.1,conn-port=8030,bus=ehci.0 &&
 
-sleep 5
+sleep 5 &
 
 echo "Starting iPhone emulator..."
 ./qemu-system-aarch64 -M t8030,trustcache=iPhone11_8_iPhone12_1_14.0_18A5351d_Restore/Firmware/038-44135-124.dmg.trustcache,ticket=root_ticket.der,sep-fw=sep-firmware.n104.RELEASE.new.img4,sep-rom=AppleSEPROM-Cebu-B1,kaslr-off=true,usb-conn-type=ipv4,usb-conn-addr=127.0.0.1,usb-conn-port=8030 \
